263542ff56f516b52a137e0032d0c0a3f167b56f
AJoHUB <ajinfotech93@gmail.com>
2014-08-11 02:18:05 +0530

    updated js

--- GCShare/WebContent/resources/default/script/GuyCarpenter.js
+++ GCShare/WebContent/resources/default/script/GuyCarpenter.js
@@ -1,60 +1,101 @@
-$(document).ready(
-		
-		
-		function()
-		{
-			var i=0;
-				$("#reports-section-plus").click(function()
-				{
-					/*if(i%)*/
-					$("#reports-section-minus").css("display","block");
-					$("#reports-section-hori").css("display","block");
-					$("#reports-section-plus").css("display","none");
-					//i=i+1;
+$(document)
+		.ready(
+
+				function() {
+					var i = 0;
+					$(".reports-n-achievements-n-trainings-toggle-button")
+							.click(
+									function() {
+										var button_id = $(this).attr('id');
+										var a = button_id.split("-");
+										var button_text_append = a[0] + "-"
+												+ a[1];
+										var current_status = $(
+												"#" + button_text_append
+														+ "-minus").css(
+												"display");
+										var hori_color = $(
+												"#" + button_text_append
+														+ "-hori").css(
+												"background-color");
+										if (current_status == "none") {// display
+											// hori
+											// section
+											$(
+													".reports-n-achievements-n-trainings-toggle-button-minus")
+													.css("display", "none");
+											$(
+													".reports-n-achievements-n-trainings-toggle-button-plus")
+													.css("display", "block");
+											$(
+													"#" + button_text_append
+															+ "-plus").css(
+													"display", "none");
+											$(
+													"#" + button_text_append
+															+ "-minus").css(
+													"display", "block");
+											$(
+													"#" + button_text_append
+															+ "-hori").css(
+													"display", "block");
+											$(
+													"#" + button_text_append
+															+ "-hori")
+													.siblings().css("display",
+															"none");
+											/*
+											 * $(".reports-n-achievements-n-trainings-individual-box-verti")
+											 * .css("background-color",
+											 * "transparent"); $( "#" +
+											 * button_text_append +
+											 * "-verti").css(
+											 * "background-color", hori_color);
+											 */
+											$(
+													"#" + button_text_append
+															+ "-plus").css(
+													"display", "none");
+										} else if (current_status == "block") {// hide
+											// hori
+											// section
+											$(
+													".reports-n-achievements-n-trainings-toggle-button-minus")
+													.css("display", "none");
+											$(
+													".reports-n-achievements-n-trainings-toggle-button-plus")
+													.css("display", "block");
+											$(
+													"#" + button_text_append
+															+ "-minus").css(
+													"display", "none");
+											$(
+													"#" + button_text_append
+															+ "-plus").css(
+													"display", "block");
+											$(
+													"#" + button_text_append
+															+ "-hori").css(
+													"display", "none");
+											$(
+													"#" + button_text_append
+															+ "-hori")
+													.siblings().css("display",
+															"none");
+											/*
+											 * $(".reports-n-achievements-n-trainings-individual-box-verti")
+											 * .css("background-color",
+											 * "transparent"); $( "#" +
+											 * button_text_append +
+											 * "-verti").css(
+											 * "background-color", hori_color);
+											 */
+											$(
+													"#" + button_text_append
+															+ "-minus").css(
+													"display", "none");
+										}
+
+									});
+
 				});
-				$("#reports-section-minus").click(function()
-						{
-							/*if(i%)*/
-							$("#reports-section-plus").css("display","block");
-							$("#reports-section-hori").css("display","none");
-							$("#reports-section-minus").css("display","none");
-							//i=i+1;
-						});
-				
-				$("#achievements-section-plus").click(function()
-						{
-							/*if(i%)*/
-							$("#achievements-section-minus").css("display","block");
-							$("#achievements-section-hori").css("display","block");
-							$("#achievements-section-plus").css("display","none");
-							//i=i+1;
-						});
-				$("#achievements-section-minus").click(function()
-						{
-									/*if(i%)*/
-							$("#achievements-section-plus").css("display","block");
-							$("#achievements-section-hori").css("display","none");
-							$("#achievements-section-minus").css("display","none");
-									//i=i+1;
-						});
-				$("#trainings-section-plus").click(function()
-						{
-							/*if(i%)*/
-							$("#trainings-section-minus").css("display","block");
-							$("#trainings-section-hori").css("display","block");
-							$("#trainings-section-plus").css("display","none");
-							//i=i+1;
-						});
-				$("#trainings-section-minus").click(function()
-						{
-									/*if(i%)*/
-							$("#trainings-section-plus").css("display","block");
-							$("#trainings-section-hori").css("display","none");
-							$("#trainings-section-minus").css("display","none");
-									//i=i+1;
-						});
-
-
-				
-
-});
