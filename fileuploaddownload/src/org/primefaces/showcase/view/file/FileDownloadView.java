08b201e08a6f02d31f1ac3a1384882df17e0f56e
AJoHUB <ajinfotech93@gmail.com>
2014-08-13 10:57:13 +0530

    upload folder commit

--- fileuploaddownload/src/org/primefaces/showcase/view/file/FileDownloadView.java
+++ fileuploaddownload/src/org/primefaces/showcase/view/file/FileDownloadView.java
@@ -1,24 +1,34 @@
 package org.primefaces.showcase.view.file;
- 
+
+import java.io.File;
 import java.io.InputStream;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
 import javax.servlet.ServletContext;
- 
+
 import org.primefaces.model.DefaultStreamedContent;
 import org.primefaces.model.StreamedContent;
- 
+
 @ManagedBean
 public class FileDownloadView {
-     
-    private StreamedContent file;
-     
-    public FileDownloadView() {        
-        InputStream stream = ((ServletContext)FacesContext.getCurrentInstance().getExternalContext().getContext()).getResourceAsStream("/resources/default/images/expand.jpg");
-        file = new DefaultStreamedContent(stream, "images/jpg", "dwn_expand.jpg");
-    }
- 
-    public StreamedContent getFile() {
-        return file;
-    }
+
+	private StreamedContent file;
+
+	public FileDownloadView() {
+
+		int count = new File(
+				"C:/Users/Asad/workspace/fileuploaddownload/WebContent/resources/default/uploads")
+				.list().length;
+		System.out.println("Number of file : " + count);
+
+		InputStream stream = ((ServletContext) FacesContext
+				.getCurrentInstance().getExternalContext().getContext())
+				.getResourceAsStream("/resources/default/images/expand.jpg");
+		file = new DefaultStreamedContent(stream, "images/jpg",
+				"dwn_expand.jpg");
+	}
+
+	public StreamedContent getFile() {
+		return file;
+	}
 }
