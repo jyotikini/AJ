afc9b0e17e395a076c8bfd9845c4bf5c22ae2dea
AJoHUB <ajinfotech93@gmail.com>
2014-08-13 10:56:21 +0530

    file count and link

--- Datatable/src/com/corejsf/TableData.java
+++ Datatable/src/com/corejsf/TableData.java
@@ -1,5 +1,6 @@
 package com.corejsf;
 
+import java.io.File;
 import java.io.Serializable;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -7,7 +8,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
- 
+
 import javax.annotation.Resource;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
@@ -17,40 +18,50 @@ import javax.naming.NamingException;
 import javax.sql.DataSource;
 import com.corejsf.Name;
 
-@ManagedBean(name="table")
+@ManagedBean(name = "table")
 @SessionScoped
 public class TableData implements Serializable {
-	
-	@Resource(name="jdbc/whiztechaj")
-	private DataSource ds;
 
-	public List<Name> getNames() throws SQLException{
+	@Resource(name = "jdbc/whiztechaj")
+	private DataSource ds;
+	int i = 0;
+
+	public List<Name> getNames() throws SQLException {
 		System.out.println("TableData");
-		if(ds==null)
-			throw new SQLException("Can't get data source");
- 
-		//get database connection
-		Connection con = ds.getConnection();
- 
-		if(con==null)
-			throw new SQLException("Can't get database connection");
- 
-		PreparedStatement ps 
-			= con.prepareStatement("select lst_nm, fst_nm from person"); 
- 
-		//get customer data from database
-		ResultSet result =  ps.executeQuery();
- 
+		/*
+		 * if(ds==null) throw new SQLException("Can't get data source");
+		 * 
+		 * //get database connection Connection con = ds.getConnection();
+		 * 
+		 * if(con==null) throw new
+		 * SQLException("Can't get database connection");
+		 * 
+		 * PreparedStatement ps =
+		 * con.prepareStatement("select lst_nm, fst_nm from person");
+		 * 
+		 * //get customer data from database ResultSet result =
+		 * ps.executeQuery();
+		 */
+
 		List<Name> list = new ArrayList<Name>();
- 
-		while(result.next()){
-			Name cust = new Name();
- 
-			cust.setLast(result.getString("lst_nm"));
-			cust.setFirst(result.getString("fst_nm"));
-			//store all data into a List
-			list.add(cust);
+		File folder = new File(
+				"C:/Users/Asad/git/datatable/Datatable/WebContent/resources/default/uploads");
+		File[] listOfFiles = folder.listFiles();
+
+		for (int i = 0; i < listOfFiles.length; i++) {
+			if (listOfFiles[i].isFile()) {
+				Name cust = new Name();
+
+				cust.setLast(listOfFiles[i].getName());
+				cust.setFirst(listOfFiles[i].getPath());
+				// store all data into a List
+				list.add(cust);
+				System.out.println("File " + listOfFiles[i].getName());
+			} else if (listOfFiles[i].isDirectory()) {
+				System.out.println("Directory " + listOfFiles[i].getName());
+			}
 		}
+
 		return list;
 	}
 }
